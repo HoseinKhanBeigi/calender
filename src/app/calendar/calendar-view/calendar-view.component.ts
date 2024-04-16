dec78bbea1209c7a1bf98712cd3aaf9cae7f55cd
hossein <hossein@hosseins-MacBook-Pro.local>
2024-04-16 19:12:45 +0430

    fix

--- src/app/calendar/calendar-view/calendar-view.component.ts
+++ src/app/calendar/calendar-view/calendar-view.component.ts
@@ -1,18 +1,8 @@
-import {
-  AfterRenderRef,
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  QueryList,
-  ViewChild,
-  ViewChildren,
-} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { TIME_SLOTS } from './time-slots';
 import { AppointmentComponent } from '../appointment/appointment.component';
-import { CanvasDragDropComponent } from '../canvas-drag-drop/canvas-drag-drop.component';
 import { AddAppointmentComponent } from '../add-appointment/add-appointment.component';
 import { DatePickerComponent } from '../date-picker/date-picker.component';
 import { ScheduleService } from '../../schedule.service';
@@ -20,9 +10,6 @@ import { Schedule } from '../../schedule.model';
 import { NgForOf } from '@angular/common';
 import {
   CdkDrag,
-  CdkDragDrop,
-  CdkDragEnter,
-  CdkDragExit,
   CdkDragPlaceholder,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
@@ -34,7 +21,6 @@ import {
     CdkDrag,
     CdkDragPlaceholder,
     CdkDropList,
-    CanvasDragDropComponent,
     AppointmentComponent,
     AddAppointmentComponent,
     DatePickerComponent,
@@ -43,13 +29,11 @@ import {
   templateUrl: './calendar-view.component.html',
   styleUrl: './calendar-view.component.scss',
 })
-export class CalendarViewComponent implements AfterViewInit {
+export class CalendarViewComponent {
   timeSlots = TIME_SLOTS;
   schedules: Schedule[] = [];
   startTime: number = 0;
-
   @ViewChild('myList') myList: ElementRef | undefined;
-  @ViewChildren('listItem') listItems: QueryList<ElementRef> | undefined;
   date: Date | undefined;
   constructor(
     public dialog: MatDialog,
@@ -59,16 +43,6 @@ export class CalendarViewComponent implements AfterViewInit {
 
   ngOnInit() {
     console.log('...هدفم خیره\n', '...نیتم خیره\n', '...برای امر خیره\n');
-
-    // const element = this.slotTimeLine?.nativeElement;
-    // console.log(element.children);
-
-    // [...element.children].addEventListener('dragleave', () => {
-    //   console.log('hi...');
-    //   // You might want to set some data here
-    //   // e.dataTransfer.setData('text/plain', 'This text may be dragged');
-    // });
-
     this.scheduleService.getSchedules().subscribe((schedules) => {
       this.route.params.subscribe((params) => {
         const year = params['year'];
@@ -86,26 +60,8 @@ export class CalendarViewComponent implements AfterViewInit {
       });
     });
   }
-  ngAfterViewInit() {
-    this.myList?.nativeElement.children[0].addEventListener('', () => {
-      console.log('hi...');
-      // You might want to set some data here
-      // e.dataTransfer.setData('text/plain', 'This text may be dragged');
-    });
-  }
-
-  drop(event: CdkDragDrop<string[]>) {}
-
-  onDragEnter(event: any, time: any) {}
-
-  onDragExit(event: any, slot: any) {
-    // Optional: Action when the draggable element leaves the drop zone
-  }
-
-  onDragStart(event: DragEvent) {}
 
   handleClick(e: any) {
-    // this.startTime = Math.floor(e.pageY / 60);
     this.dialog.open(AddAppointmentComponent, {
       data: { date: this.date },
     });
